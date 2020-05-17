094f30ba2af83bd42484d30024ac3324adc0516d
Isofruit <philippmdoerner@web.de>
2020-05-17 12:25:46 +0200

    Moved creation of logger out of main(). Minor refactoring in main(). Changed logged test-finish message out of a whim.

--- sfdbtester/__main__.py
+++ sfdbtester/__main__.py
@@ -5,45 +5,47 @@ from datetime import datetime
 from sfdbtester.common.argparser import get_args
 
 
-def main(args):
-	# Get SFDB File Object
-	sfdb_new = SFDBContainer.from_file(args.SFDBFile)
-
-	# Initialize logging
-	now = f'{datetime.now():%y-%m-%d_%H%M%S}'
-	log_file_path = f'{args.SFDBFile[:-5]}_{now}_log.txt'
+def create_main_logger(log_filepath):
 	LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-	handler = logging.FileHandler(log_file_path, 'w', 'utf-8')
+	handler = logging.FileHandler(log_filepath, 'w', 'utf-8')
 	logging.basicConfig(level=logging.DEBUG,
 						format=LOG_FORMAT,
 						handlers=[handler])
-	logger = logging.getLogger('main')
+	return logging.getLogger('main')
+
+
+def main(args):
+	# Initialize logging
+	now = f'{datetime.now():%y-%m-%d_%H%M%S}'
+	log_filepath = f'{args.SFDBFile[:-5]}_{now}_log.txt'
+	logger = create_main_logger(log_filepath)
 	logger.info(f'LOG FILE FOR SFDB FILE : {args.SFDBFile}')
 	logger.info('Indices in this log start from 1')
 
 	warning_counter = 0
 
+	# Get SFDB File Object
+	sfdb_new = SFDBContainer.from_file(args.SFDBFile)
+
 	# Perform Tests on SFDB file
 	logger.info('START FORMAT TEST')
 	warning_counter += st.test_sfdb_format(sfdb_new)
-	logger.info('FORMAT TEST FINISHED')
+	logger.info('FINISHED FORMAT TEST')
 
 	logger.info('START DATATYPE TEST')
 	warning_counter += st.test_sfdb_datatypes(sfdb_new)
-	logger.info('DATATYPE TEST FINISHED')
+	logger.info('FINISHED  DATATYPE TEST')
 
 	if args.re:
 		logger.info('START REGEX TEST')
 		warning_counter += st.test_sfdb_against_regex(sfdb_new, args.re)
-		logger.info("REGEX TEST FINISHED")
+		logger.info("FINISHED  REGEX TEST")
 
 	if args.comparison_sfdb:
 		logger.info('START COMPARISON TEST')
 		sfdb_old = SFDBContainer.from_file(args.comparison_sfdb)
-		warning_counter += st.compare_sfdb_files(sfdb_new, sfdb_old,
-																args.ex_lines1, args.ex_lines2,
-																args.ex_col)
-		logger.info('COMPARISON TEST FINISHED')
+		warning_counter += st.compare_sfdb_files(sfdb_new, sfdb_old, args.ex_lines1, args.ex_lines2, args.ex_col)
+		logger.info('FINISHED COMPARISON TEST')
 
 	if args.write:
 		no_dupl_sfdb_file = args.SFDBFile[:-5] + '_no_duplicates.sfdb'
@@ -53,7 +55,7 @@ def main(args):
 	# Finish logging
 	logger.info('Done')
 	print(f'The file caused {warning_counter} warning-messages.')
-	print(f'Logfile written to {log_file_path}.\nDone')
+	print(f'Logfile written to {log_filepath}.\nDone')
 
 
 if __name__ == '__main__':
