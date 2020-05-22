845b9bb2530d317d7ecb7294d70e81ec82e90e89
Isofruit <philippmdoerner@web.de>
2020-05-22 07:55:06 +0200

    utilities module now has code coverage!
    - Utilities module was not tested
    - Unittesting is required for robustness
    - As a result of testing the utilities functions were refactored for better robustness

---
+++ sfdbtester/tests/test_utilities.py
@@ -0,0 +1,20 @@
+import unittest as ut
+from sfdbtester.common import utilities
+
+
+class TestUtilities(ut.TestCase):
+    def test_get_resource_filepath_resource_in_project(self):
+        test_file = 'empty.sfdb'
+        test_filepath = utilities.get_resource_filepath(test_file)
+        contained_in_expected_output = '/sfdbtester/resources/empty.sfdb'
+        self.assertTrue(contained_in_expected_output in test_filepath)
+
+    def test_get_resource_filepath_nonexistant_resource(self):
+        test_file = 'ThisResourceDoesNotexist'
+        with self.assertRaises(ValueError):
+            utilities.get_resource_filepath(test_file)
+
+    def test_get_resource_filepath_dir(self):
+        test_file = 'test_dir'
+        with self.assertRaises(ValueError):
+            utilities.get_resource_filepath(test_file)
