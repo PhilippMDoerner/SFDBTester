cac5eb275c022acaa4ef34cf474151c6464d54f8
Isofruit <philippmdoerner@web.de>
2020-05-25 09:18:02 +0200

    Added trackable empty directory

--- sfdbtester/tests/test_utilities.py
+++ sfdbtester/tests/test_utilities.py
@@ -6,8 +6,12 @@ class TestUtilities(ut.TestCase):
     def test_get_resource_filepath_resource_in_project(self):
         test_file = 'empty.sfdb'
         test_filepath = utilities.get_resource_filepath(test_file)
-        contained_in_expected_output = '/sfdbtester/resources/empty.sfdb'
-        self.assertTrue(contained_in_expected_output in test_filepath)
+
+        dir1 = "sfdbtester"
+        dir2 = "resources"
+        self.assertTrue(dir1 in test_filepath)
+        self.assertTrue(dir2 in test_filepath)
+        self.assertTrue(test_file in test_filepath)
 
     def test_get_resource_filepath_nonexistant_resource(self):
         test_file = 'ThisResourceDoesNotExist'
